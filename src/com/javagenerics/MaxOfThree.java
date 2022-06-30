0a21a6adab3ff886b2c5239135cc384cd73dd4e5
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-30 09:20:54 +0530

    Checking for largest String

--- src/com/javagenerics/MaxOfThree.java
+++ src/com/javagenerics/MaxOfThree.java
@@ -6,12 +6,12 @@ public class MaxOfThree {
     public static void largest() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter 1st Number: ");
-        Double first = input.nextDouble();
+        String first = input.next();
         System.out.println("Enter 2nd Number: ");
-        Double second = input.nextDouble();
+        String second = input.next();
         System.out.println("Enter 3rd Number: ");
-        Double third = input.nextDouble();
-        Double max = first;
+        String third = input.next();
+        String max = first;
         if (second.compareTo(max)>0)
             max = second;
         if (third.compareTo(max)>0)
