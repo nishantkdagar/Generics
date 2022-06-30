2bb3cf28dbbd99acfc2d876cb5c53c7b43680866
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-30 09:27:35 +0530

    Using generics for comaprison

--- src/com/javagenerics/MaxOfThree.java
+++ src/com/javagenerics/MaxOfThree.java
@@ -1,26 +1,37 @@
 package com.javagenerics;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class MaxOfThree {
-    public static void largest() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter 1st Number: ");
-        String first = input.next();
-        System.out.println("Enter 2nd Number: ");
-        String second = input.next();
-        System.out.println("Enter 3rd Number: ");
-        String third = input.next();
-        String max = first;
+class Compare<T extends Comparable<T>>{
+
+    public T large(){
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter 1st Value: ");
+        T first = (T) inp.next();
+        System.out.println("Enter 2nd Value: ");
+        T second = (T) inp.next();
+        System.out.println("Enter 3rd Value: ");
+        T third = (T) inp.next();
+
+
+        T max = first;
         if (second.compareTo(max)>0)
             max = second;
         if (third.compareTo(max)>0)
             max = third;
-        System.out.printf("The Maximum of three numbers is: "+ max);
+        //System.out.printf("The Maximum of three numbers is: "+ max);
+        return max;
     }
 
+}
+public class MaxOfThree {
     public static void main(String[] args) {
-
-        MaxOfThree.largest();
+        Compare<Integer> largeint = new Compare<Integer>();
+        System.out.println("Largest Integer is: "+largeint.large());
+        Compare<Double> largefloat = new Compare<Double>();
+        System.out.println("Largest Float is: "+largefloat.large());
+        Compare<String> largestring = new Compare<String>();
+        System.out.println("Largest String is: "+largestring.large());
     }
 }
